8f485d9a7c7e20a0e24ad03f6f89ed2358334562
your name here <abc@test.com>
2024-05-01 15:59:15 +0530

    first commit

---
+++ app/components/Input.js
@@ -0,0 +1,29 @@
+// 'use client';
+// import React, { useState } from 'react';
+
+// function Input({ label, placeholder, className,  }) {
+
+//      const [input, setInput] = useState('');
+
+
+// 	return (
+// 		<>
+// 			{/* {label && (
+// 				<label htmlFor="" className="label block text-xl">
+// 					{label}
+// 				</label>
+// 			)} */}
+// 			<input
+// 				type="text"
+// 				placeholder={placeholder}
+// 				name=""
+// 				id=""
+//                     value={input}
+// 				onClick={handleInput}
+// 				className={`input indent-3   border rounded-md focus:border-blue-400 outline-none h-10 ${className}`}
+// 			/>
+// 		</>
+// 	);
+// }
+
+// export default Input;
